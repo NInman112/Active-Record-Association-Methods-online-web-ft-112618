006a35d5c52f2ef6087f52674023ce13611004fd
Nick Inman <nick@theoatmeal.com>
2019-01-13 03:18:21 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -8,12 +8,12 @@ class Genre < ActiveRecord::Base
   end
 
   def artist_count
-    self.artists.count 
+    self.artists.count
     # return the number of artists associated with the genre
   end
 
   def all_artist_names
-    self.artists.all
+    self.artists.all.name
     # return an array of strings containing every musician's name
   end
 end
